cce76e1046051b7d5e84ab6e5a1b3ad2d092f6f3
francojesusre <francojesusre@gmail.com>
2020-08-03 19:50:45 -0300

    especialidad

--- Data.Database/EspecialidadAdapter.cs
+++ Data.Database/EspecialidadAdapter.cs
@@ -57,12 +57,40 @@ namespace Data.Database
 
         protected void Insert(Especialidad especialidad)
         {
-           
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("INSERT INTO especialidades(desc_especialidad)" +
+                " VALUES (@desc_especialidad) select @@identity", sqlConnection);
+                cmdSave.Parameters.Add("@desc_especialidad", SqlDbType.VarChar, 100).Value = especialidad.Descripcion;
+                especialidad.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear Especialidad", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
         }
 
         public void Save(Especialidad especialidad)
         {
-           
+            if (especialidad.State == BusinessEntity.States.New)
+            {
+                this.Insert(especialidad);
+            }
+            else if (especialidad.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(especialidad.ID);
+            }
+            else if (especialidad.State == BusinessEntity.States.Modified)
+            {
+                this.Update(especialidad);
+            }
+            especialidad.State = BusinessEntity.States.Unmodified;
         }
     }
 }
