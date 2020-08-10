4dd0fa1316a31dcfce83abc882164efd932f9209
moonfrank <franconicolas2007@hotmail.com>
2020-08-10 17:29:57 -0300

    zcgbhdfkl

---
+++ Business.Entities/BusinessEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Business.Entities
+{
+    public class BusinessEntity
+    {
+        public int ID { get; set; }
+        public States State { get; set; }
+        public enum States
+        {
+            Deleted,
+            New,
+            Modified,
+            Unmodified
+        }
+
+        public SqlConnection Conn { get; set; }
+        public BusinessEntity()
+        {
+            this.Conn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=tp2_net;Integrated Security=True");
+            //this.Conn = new SqlConnection("Data Source=localhost;Initial Catalog=tp2_net;Integrated Security=True");
+            this.State = States.New;
+        }
+    }
+
+
+}
